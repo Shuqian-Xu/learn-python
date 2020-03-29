4f7a2bcef68b20b3e36ba84b4f60814763f2d4ba
Shuqian Xu <xshuqian@gmail.com>
2020-03-29 19:15:01 +0200

    Calculate average length

--- review.py
+++ review.py
@@ -7,8 +7,14 @@ with open("reviews.txt", "r") as f:
         count += 1
         if count % 1000 == 0:
             print(len(data))
+print("There are a total of", len(data), "lines.")
 
-print(len(data))
-print(data[0])
-print("-----------------")
-print(data[1])
+# print(len(data))
+# print(data[0])
+# print("-----------------")
+# print(data[1])
+
+sumLength = 0
+for d in data:
+    sumLength += len(d)
+print("The average is", sumLength / len(data))
