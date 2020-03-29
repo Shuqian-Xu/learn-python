968c4bc65eefe8493e8a8abd7539dc97aec89bbb
Shuqian Xu <xshuqian@gmail.com>
2020-03-29 18:29:18 +0200

    Added the ability to count the number of guesses

--- guessNumber.py
+++ guessNumber.py
@@ -1,11 +1,14 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
+
 while True:
+    count += 1
     guess = input("Please input your guess: ")
     guess = int(guess)
     if guess == r:
-        print("You have made the right guess!")
+        print("You have made the right guess with", count, "guesses!")
         break
     elif guess < r:
         print("You guess is smaller than the number!")
